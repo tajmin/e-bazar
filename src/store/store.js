c6786758a691a1e05099f01d1e8d1ebf60cb68a9
Md. Tajminur Rahman <tajminur.aiub@gmail.com>
2022-09-20 22:03:22 +0600

    redux persist integrated for local storage persistance

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,21 @@
-import { compose, createStore, applyMiddleware } from "redux";
+import { applyMiddleware, compose, createStore } from "redux";
 import logger from "redux-logger";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { rootReducer } from "./root-reducer";
 
 const middleWares = [logger];
 
 const composeEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, composeEnhancers);
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(persistedReducer, undefined, composeEnhancers);
+
+export const persistor = persistStore(store);
